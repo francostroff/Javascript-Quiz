382abaee8bf5edb881bbaa4d80a5a3906f129bf8
francostroff <116038334+francostroff@users.noreply.github.com>
2023-01-13 00:07:53 +0000

    initial commit

---
+++ starter/questions.js
@@ -0,0 +1,61 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a','b','c','d','e','f','g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A', 'B', 'C', 'D','E','F', 'G', 'H','I', 'J','K','L','M','N','O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+
+// Function to prompt user for password options
+let lenght = prompt("How many characters would you like in your password?");
+if (lenght>64) {
+  alert("max lenght 64 characters")
+}
+let special = prompt("Do you need any special characters - y or n yes or no?");
+if (special === "y"){
+  alert(`OK`);
+}
+let numbers = prompt("Would you like to include NUMBERS?");
+{
+alert("Welcome!");
+}
+
+
+// Function to generate password with user input
+function generatePassword() {
+const lenght= 24;
+const allCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a','b','c','d','e','f','g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+  'A', 'B', 'C', 'D','E','F', 'G', 'H','I', 'J','K','L','M','N','O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+let password = "";
+for (let i=0; i < lenght; i++){
+  const character = allCharacters[Math.floor(Math.random()*allCharacters.length)];
+password += character;
+}
+console.log (password);
+return password;
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
