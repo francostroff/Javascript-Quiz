06ed4e217ba284a7ec320f78d2f8e585f6b454b3
francostroff <116038334+francostroff@users.noreply.github.com>
2023-01-17 21:51:15 +0000

    fixed start screen questions container and timer

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,51 +1,54 @@
 //need a for loop to change questions and print choices
-console.log("Here are the questions");
-let myQuestions = [
-  { numb:1,
-    question: "What is a string?",
-    answers: {
-      a: '3',
-      b: '5',
-      c: '115',
-    },
-    correctAnswer: 'b'
+let questions = [
+  { question: "What is the DOM?",
+    answers: [
+       'An array of objects.',
+      'The Document Object Module.',
+     'A new computer virus. ',
+    ],
+    correctAnswer: 'An array of objects.'
     
   },
   {
-    numb:2,
-    question: "What is an array?",
-    answers: {
-      a: 'a fish',
-      b: 'a crypto currency',
-      c: 'multiple objects inside a variable',
-    },
-    correctAnswer: 'c'
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: [
+       '<javascript>',
+      '<scripting>',
+      '<script>',
+    ],
+    correctAnswer: '<script>'
   },
   { 
-    numb:3,
-    question: "What is the DOM?",
-    answers: {
-      a: 'Document Object Module',
-      b: 'Champagne brand',
-      c: 'A new Nasa project',
-    },
-    correctAnswer: 'a'
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: [
+       'The head section',
+       'Both the head section and the body section are correct',
+       'The body section',
+    ],
+    correctAnswer: 'Both the head section and the body section are correct'
   },
   {
-    numb:4,
-    question: "what is a function?",
-    answers: {
-      a: '3.333333',
-      b: '30',
-      c: '29',
-    },
-    correctAnswer: '3.333333'
+    question: "How do you write Hello World in an alert box?",
+    answers: [
+        'msgBox(HelloWorld)',
+        'alert(HelloWorld)',
+        'msg(HelloWorld)',
+    ],
+    correctAnswer:  'msgBox(HelloWorld)'
   },
+{
+  question: "How do you create a function in JavaScript?",
+  answers: [
+     'function = myFunction()',
+     'function myFunction()',
+     'function:myFunction()',
+  ],
+  correctAnswer: 'function myFunction()'
+},
 
 ];
 
+let wrongAnswers = [];
+let correctAnswers = [];
+console.log(questions);
 
-function clicked() {
-  var answerText = document.getElementById("answer");
-  answerText.style.display = "inline";
-}
--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,154 +1,50 @@
-
-// // function for showing the question
-// function showQuestion(question1) {
-//   // code
-// }
-// // call the function
-// showQuestion(question1);
-// console.log (question1)
-          // Hide
-  
-// let start = document.querySelector('#start');
-
-
-
+console.log(document.body);
+console.log(document.body.children);
+console.log(questions)
+var myInterval;
+let myQuestions = document.querySelector("#questions");
+let answerContainer = document.getElementById("#choices");
+let correctAnswer = [];
+let selectedAnswers = {};
+let index;
+var resultsContainer = document.getElementById('results');
+var results=[];
+let startScreen = document.getElementById("start-screen");
+var submitButton = document.getElementById('submit');
+var secondsLeft = document.getElementById("time");
 //timer countdown
-let timer = 0
-let maxTicks = 91;
+let maxTicks = 90;
+let minTicks = 0
 let tickCount = 0;
-let tick = function()
-{if (tickCount === maxTicks)
-    {
-   // Stops the interval.
-        clearInterval(myInterval);
-        return;
-    }
-   // The particular code you want to excute on each tick 
-    document.getElementById("time").innerHTML = (maxTicks - tickCount);
-    tickCount++;
-};
-let myInterval = setInterval(tick, 1000);
+let tick = function(){
+  maxTicks--
+  secondsLeft.textContent=maxTicks
+}
+// The particular code you want to excute on each tick 
+    // document.getElementById("time").innerHTML = (maxTicks - tickCount);
+    // tickCount++;
 
 //starts the quiz
 function startQuiz() {
-  document.getElementById("start-screen").innerHTML = "";
- }
- 
+  myInterval = setInterval(tick, 1000);
+  startScreen.setAttribute("class","hide")
+  myQuestions.removeAttribute("class")
+}
+
+function showQuestion() {
+ document.getElementById("questions").style.display = "block"
+}
+
 //show question 1
-function startQuestion(){
-  document.getElementsByClassName(".hide").innerHTML = "";
- }
-  console.log(questions);
-
-// titleDiv.textContent = questions;
-// showQuestion(questions);
-// let titleDiv = document.getElementById('question-title');
-// titleDiv.textContent = questions-title;
-// showQuestion(question-title);
-
-// let time = document.querySelector('time');
-//  time.addEventListener('click', function() {
-//  	let i = 0;
-	
-// 	setInterval(function() {
-// 		alert(++i);
-//  }, 1000);
-//  });
-
-//  var questions = [
-//   {
-//     question: "What is a string?",
-//     answers: {
-//       a: 'Line of text',
-//       b: 'Strings are for storing and manipulating text',
-//       c: 'A special variable, which can hold more than one value:',
-//     },
-//     correctAnswer: 'b'
-    
-//   },
-//   {
-//     question: "What is an array?",
-//     answers: {
-//       a: '3',
-//       b: '5',
-//       c: '10',
-//     },
-//     correctAnswer: 'c'
-//   },
-//   {
-//     question: "What is an object?",
-//     answers: {
-//       a: '3',
-//       b: '22.5',
-//       c: '35.5',
-//     },
-//     correctAnswer: 'b'
-//   },
-//   {
-//     question: "what is a function?",
-//     answers: {
-//       a: '3.333333',
-//       b: '30',
-//       c: '29',
-//     },
-//     correctAnswer: '3.333333'
-//   },
-
-// ];
-
-
-
-// start.addEventListener('click', function() {
-// 	let i = 0;
+function RespondClick() {
+  document.getElementById("questions").innerHTML +=
+            "Click Event" + "<br>";
+}
+//  start.addEventListener('click', function() {
 // });
-  
-// start.addEventListener("click", function(){ console.log("Start Quiz"); });
-//  ("start").click( function(){
-//   var counter = 20;
-//   setInterval(function() {
-//    counter--;
-//     if (counter >= 0) {
-//        span = document.getElementById("count");
-//        span.innerHTML = "start";
-//   }
-//  if (counter === 0) {
-//       alert('sorry, out of time');
-//         clearInterval(counter);
-//       }
-//     }, 1000);
-//  });
+  var resultsContainer = document.getElementById('results');
+  var results=[];
 
-// var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
-// var todoList = document.querySelector("#todo-list");
-// renderTodos();
-
-// function renderTodos() {
-//   // Clear todoList element and update todoCountSpan
-//   todoList.innerHTML = "";
-//   todoCountSpan.textContent = todos.length;
-
-//   // Render a new li for each todo
-//   for (var i = 0; i < todos.length; i++) {
-//     var todo = todos[i];
-
-//     var li = document.createElement("li");
-//     li.textContent = todo;
-//     todoList.appendChild(li);
-//   }
-// }
-
-
-   
-  
-
-
-
-
-// 
-var resultsContainer = document.getElementById('results');
-// const results = document.querySelector("#results")
-//console.log(resultsContainer);
-var submitButton = document.getElementById('submit');
 
 //generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
@@ -157,9 +53,10 @@ function generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton
 
 function showQuestions(questions, quizContainer){
     // we'll need a place to store the output and the answer choices
-    var output = [myQuestions.userAnswer];
+    var output = [questions.userAnswer];
     var answers = "";
     console.log(output);
+    console.log(answers)
 
     // for each question...
     for(var i=0; i<questions.length; i++){
@@ -234,7 +131,23 @@ function showQuestions(questions, quizContainer){
   }
 
 }//get input value
-function getInputValue(){
+function returnText(){
+  let input = document.getElementById("userInput").value;
+  localStorage.setItem('input', input)
+  console.log(input)
+  alert (input)
 
 }
-console.log();
+function func() {
+const rightAnswers = ["b","c","a","a","b"];
+const copy = [];
+
+// rightAnswers.forEach(function (rightAnswers) {
+// copy.push(rightAnswers);
+// });
+
+console.log(copy);
+
+}
+
+
