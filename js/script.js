a6ebca89e75c10f35f06487df395449d492818fe
francostroff <116038334+francostroff@users.noreply.github.com>
2023-01-24 10:47:55 +0000

    fixing js

--- js/script.js
+++ js/script.js
@@ -41,67 +41,76 @@ function showQuestion() {
   var choiceButton = document.createElement ("button");
   choiceButton.setAttribute("class","choice");
   choiceButton.setAttribute("value",choice);
-  console.log(choice);
   choiceButton.textContent = i+1+". "+ choice;
-  // choiceButton.onclick = questionClick
-  console.log(choiceButton.textContent);
+  choiceButton.onclick = choiceClick;
   choiceContainer.appendChild(choiceButton);
- })
+ });
 }
 // need to write a function that check whether answers click is right or wrong
 // if its wrong subtract time and move to next question (need an if statement also need currentIndex++)
 // if its right move to the next question (currentIndex++)
 //show question 1
 function choiceClick() {
+  console.log("Hello world!")
   if (this.value !== questions[currentIndex].correctAnswer) {
     maxTicks -= 15 
+  }
+  if (maxTicks < 0) {maxTicks=0} {
+    secondsLeft.textContent = maxTicks
+  };
+  currentIndex++;
+  if (currentIndex === questions.length){
+     stopGame();
+  } 
+  else {
+   showQuestion();
   }
 }
 //  start.addEventListener('click', function() {
 // });
-  var resultsContainer = document.getElementById('results');
-  var results=[];
+  // var resultsContainer = document.getElementById('results');
+  // var results=[];
 
 
-function generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton){
-}
+// function generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton){
+// }
 
-function showQuestions(questions, quizContainer){
-    // we'll need a place to store the output and the answer choices
-    var output = [questions.userAnswer];
-    var answers = "";
-    console.log(output);
-    console.log(answers);
+// function showQuestions(questions, quizContainer){
+//     // we'll need a place to store the output and the answer choices
+//     var output = [questions.userAnswer];
+//     var answers = "";
+//     console.log(output);
+//     console.log(answers);
 
-    // for each question...
-    for(var i=0; i<questions.length; i++){
+//     // for each question...
+//     for(var i=0; i<questions.length; i++){
       
-      // first reset the list of answers
-      answers = [];
+//       // first reset the list of answers
+//       answers = [];
 
-      // for each available answer...
-      for(letter in questions[i].answers){
+//       // for each available answer...
+//       for(letter in questions[i].answers){
 
-        // ...add an html radio button
-        answers.push(
-          '<label>'
-            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-            + letter + ': '
-            + questions[i].answers[letter]
-          + '</label>'
-        );
-      }
+//         // ...add an html radio button
+//         answers.push(
+//           '<label>'
+//             + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+//             + letter + ': '
+//             + questions[i].answers[letter]
+//           + '</label>'
+//         );
+      // }
 
       // add this question and its answers to the output
-      output.push(
-        '<div class="question">' + questions[i].question + '</div>'
-        + '<div class="answers">' + answers.join('') + '</div>'
-      );
-    }
+  //     output.push(
+  //       '<div class="question">' + questions[i].question + '</div>'
+  //       + '<div class="answers">' + answers.join('') + '</div>'
+  //     );
+  //   }
 
-    // finally combine our output list into one string of html and put it on the page
-    quizContainer.innerHTML = output.join('');{
-  }
+  //   // finally combine our output list into one string of html and put it on the page
+  //   quizContainer.innerHTML = output.join('');{
+  // }
 
   function showResults(questions, quizContainer, resultsContainer){
     
@@ -138,14 +147,14 @@ function showQuestions(questions, quizContainer){
   }
 
   // show questions right away
-  showQuestions(questions, quizContainer);
+  // showQuestions(questions, quizContainer);
   
   // on submit, show results
   submitButton.onclick = function(){
     showResults(questions, quizContainer, resultsContainer);
   }
 
-}//get input value
+//get input value
 function returnText(){
   let input = document.getElementById("userInput").value;
   localStorage.setItem('input', input)
@@ -153,16 +162,16 @@ function returnText(){
   alert (input)
 
 }
-function func() {
-const rightAnswers = ["b","c","a","a","b"];
-const copy = [];
+// function func() {
+// const rightAnswers = ["b","c","a","a","b"];
+// const copy = [];
 
 // rightAnswers.forEach(function (rightAnswers) {
 // copy.push(rightAnswers);
 // });
 
-console.log(copy);
 
-}
+
+
 
 
